aeccb8f22770e93f2bb0eaaba972156bc0453bfc
arsenpahi <arsen.pahi@gmail.com>
2016-11-26 17:00:07 +0200

    Update gallery.php

--- gallery.php
+++ gallery.php
@@ -69,7 +69,7 @@
 		<div id="gallery" class="box">
 			<div class="container">
 				<h2 class="title">Notre galerie</h2>
-				<p class="descr">Thus looks our halls for training</p>
+				<p class="descr">Voici nos salles pour l'entraînement physique.</p>
 				
 				<div class="images">
 					<img src="img/gallery1.jpg" alt=""/>
